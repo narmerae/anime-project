b42cfe5e38f10cdda5cfe7d6397f1f14b9febdfe
NARMER\Narmi <kujundzicmatej64@gmail.com>
2024-08-03 18:48:42 +0200

    chore(image config): configuring host image

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['cdn.myanimelist.net']
+    domains: ['cdn.myanimelist.net', 'm.media-amazon.com', 'imgsrv.crunchyroll.com']
   }
 };
 
