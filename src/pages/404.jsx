9c8620178bfe2b881962743873f994eb2e1db628
NARMER\Narmi <kujundzicmatej64@gmail.com>
2024-08-03 18:49:02 +0200

    feat(pages): error 404 page beautified

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 import RootLayout from "@/pages/components/RootLayout";
 import Link from "next/link";
 
 const error404 = () => {
     return (
-        <div className={"text-center d-flex justify-content-center align-items-center"}>
+        <div className={"text-center d-flex justify-content-center flex-column align-items-center vh-100"}>
             <h1>Error 404 </h1>
             <p>Page doesn&apos;t exist!</p>
             <Link href={"/"}>Home</Link>
